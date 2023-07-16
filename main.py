4c3d7bd085eae7af7e43791ae95daeb81456f50f
stellaskyler <69475868+stellaskyler@users.noreply.github.com>
2023-07-15 20:35:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from scrapy.crawler import CrawlerProcess
 from spiders.street_fighter_spider import StreetFighterSpider
 
-# Start the spider
+# Create a CrawlerProcess and configure it with settings
 process = CrawlerProcess(settings=StreetFighterSpider.custom_settings)
 process.crawl(StreetFighterSpider)
 process.start()
